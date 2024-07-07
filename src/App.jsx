9b77183da4fb706631fd65d8a3b721fe62837b6d
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-07 21:28:06 +0530

    Feat: add Routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-imp
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <div className='text-9xl'>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/cart" element={<CartPage />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
