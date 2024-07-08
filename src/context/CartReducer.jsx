fd167b1d829170520f5ebaa5a00fb201e733ef01
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-08 08:22:36 +0530

    feat: add reducer and set payload data and type on action

--- src/context/CartReducer.jsx
+++ src/context/CartReducer.jsx
@@ -1,8 +1,9 @@
-
-export default function CartReducer({state,action}) {
-  switch(action){
-
+export function CartReducer(state, action) {
+  switch (action.type) {
+    case "SET_PRODUCTS":
+      return { ...state,
+         products: action.payload, };
     default:
-        return state
+      return state;
   }
 }
