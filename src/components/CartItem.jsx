1e0df18f9bd525feed3b3535ac3a15e22a6e9495
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-09 10:28:46 +0530

    style: cart page ui ready

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function CartItem({ product }) {
+  return (
+    <div className="justify-between mb-6 rounded-lg bg-slate-700 p-6 shadow-xl sm:flex sm:justify-start">
+      <img
+        src={product.image}
+        alt="product-image"
+        className="w-full rounded-lg sm:w-14"
+      />
+      <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+        <div className="mt-5 sm:mt-0">
+          <h2 className="text-lg font-bold text-gray-900">{product.title}</h2>
+          <p className="mt-1 text-xs text-gray-700">36EU - 4US</p>
+        </div>
+        <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+          <div className="flex items-center border-gray-100">
+            <span className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50">
+              {" "}
+              -{" "}
+            </span>
+            <input
+              className="h-8 w-8 border bg-white text-center text-xs outline-none"
+              type="number"
+              value="2"
+              min="1"
+            />
+            <span className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50">
+              {" "}
+              +{" "}
+            </span>
+          </div>
+          <div className="flex items-center space-x-4">
+            <p className="text-sm">{product.price} $</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import CartItem from "../components/CartItem";
+import CartTotal from "../components/CartTotal";
+import { Cart } from "../context/CartContext";
 
 function CartPage() {
+  const {
+    state: { cart },
+  } = useContext(Cart);
   return (
     <>
-    <h1>Cart page</h1>
-
+      <div className="bg-gray-100 py-20">
+        <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+        <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+          <div className="rounded-lg md:w-2/3">
+            {cart?.map((prod) => {
+              return <CartItem product={prod} key={prod.id} />;
+            })}
+          </div>
+          <CartTotal />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default CartPage
+export default CartPage;
