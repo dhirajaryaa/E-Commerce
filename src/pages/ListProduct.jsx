8387abf0a31242919d49e29fba79e4c701d0a776
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-07-10 11:34:26 +0530

    feat: all filters working

--- src/pages/ListProduct.jsx
+++ src/pages/ListProduct.jsx
@@ -6,11 +6,39 @@ import Filters from "../components/Filters";
 
 function ListProduct() {
   const navigate = useNavigate();
-
   const {
-    state: { products},
+    state: { products },
     dispatch,
+    filteredState: { rating, brand, sort, searchQuery },
   } = useContext(Cart);
+
+  const transformProducts = () => {
+    let sortedProducts = products;
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+    if (brand === "all brands") {
+      sortedProducts = sortedProducts;
+    } else {
+      sortedProducts = sortedProducts.filter((item) => item.category === brand);
+    }
+    if (rating) {
+      sortedProducts = sortedProducts.filter(
+        (item) => Math.floor(item.rating.rate) === rating
+      );
+    }
+    if (searchQuery) {
+      if (searchQuery.length === 0) {
+        sortedProducts = sortedProducts;
+      }
+      sortedProducts = sortedProducts.filter((item) =>
+        item.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+    return sortedProducts;
+  };
 
   const addToCart = (prod) => {
     dispatch({ type: "SET_PRODUCTS_ON_CART", payload: prod });
@@ -22,18 +50,23 @@ function ListProduct() {
 
   return (
     <section className="text-black body-font">
-      <div className="container py-10 mx-auto px-8 sm:px-24 md:px-4">
-      <Filters />
-        <div className="flex flex-wrap gap-6 justify-evenly md:justify-between">
-          {products &&
-            products.map((product) => (
+      <div className="container py-10 mx-auto px-8 sm:px-24 md:px-4 flex flex-col items-center">
+        <Filters />
+        <div className="flex flex-wrap gap-6 w-72 sm:w-full justify-center">
+          {transformProducts().length != 0 ? (
+            transformProducts().map((product) => (
               <Product
                 key={product.id}
                 product={product}
                 addToCart={addToCart}
                 viewProduct={viewProduct}
               />
-            ))}
+            ))
+          ) : (
+            <h2 className="text-slate-800 text-2xl text-center">
+              No Product match!
+            </h2>
+          )}
         </div>
       </div>
     </section>
